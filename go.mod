0b13972c40f8ca4c0133c09de2627f49af363d41
Nazemrap <corentin.damas@gmail.com>
2023-11-29 15:41:24 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Corentin-Damas/pokedexcli
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello world")
+}
+
+// https://www.youtube.com/watch?v=8yrmAGcCnKg
+
+// go build
+// ./pokedexcli
